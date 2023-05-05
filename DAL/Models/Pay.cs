be42fdd7502bf4a7e3853c5c76b8a9ea83fa70b0
JayedChowdhuryRabby <19-40893-2@student.aiub.edu>
2023-05-05 21:01:29 +0600

    hh

--- DAL/Models/Pay.cs
+++ DAL/Models/Pay.cs
@@ -11,7 +11,7 @@ namespace DAL.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public float Amount { get; set; }
-        // public int OrderId { get; set; }
-        //public virtual Order Order { get; set; }
+        public int orderId { get; set; }
+        public virtual Order Order { get; set; }
     }
 }
